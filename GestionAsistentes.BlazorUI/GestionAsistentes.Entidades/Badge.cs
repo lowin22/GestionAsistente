eafc52d44512c773f19a1caddd8906170c67b421
lowin22 <edwin.loria@ucr.ac.cr>
2024-10-12 19:36:03 -0600

    que saico soy yo
    ya funca mamapichas

---
+++ GestionAsistentes.BlazorUI/GestionAsistentes.Entidades/Badge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAsistentes.Entidades
+{
+    public class Badge
+    {
+        public int BadgeID { get; set; }
+        public string Accesos { get; set; }
+        public string Horario { get; set; }
+        public int UnidadID { get; set; }
+        public Unidad Unidad { get; set; }
+    }
+}
