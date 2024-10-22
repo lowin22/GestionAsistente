3a1f38709c598db3b54e3e70d49ae31e24eb347e
waltaa00 <walemisanbre@gmail.com>
2024-10-22 16:57:01 -0600

    Agregar Badge

---
+++ GestionAsistentes.BlazorUI/GestionAsistentes.BlazorUI/Controlador/BadgeControlador.cs
@@ -0,0 +1,24 @@
+﻿using GestionAsistentes.Entidades;
+using GestionAsistentes.ReglasNegocio;
+
+namespace GestionAsistentes.BlazorUI.Controlador
+{
+    public class BadgeControlador
+    {
+
+        private readonly BadgeRN badgeRN;
+        public BadgeControlador()
+        {
+            badgeRN = new BadgeRN();
+        }
+        public async Task<bool> RegistrarBadge(Badge badge)
+        {
+            return await badgeRN.RegistrarBadge(badge);
+        }
+
+
+
+
+
+    }
+}
---
+++ GestionAsistentes.BlazorUI/GestionAsistentes.ReglasNegocio/BadgeRN.cs
@@ -0,0 +1,42 @@
+﻿using GestionAsistentes.AccesoDatos.EntidadesAD;
+using GestionAsistentes.AccesoDatos.Modelos;
+using GestionAsistentes.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAsistentes.ReglasNegocio
+{
+    public class BadgeRN
+    {
+        private readonly BadgeAD badgeAD;
+        
+        public BadgeRN()
+        {
+            badgeAD = new BadgeAD();
+        }
+
+        public async Task<bool> RegistrarBadge(Badge badge)
+        {
+            if (badge == null)
+            {
+                throw new ArgumentNullException(nameof(badge));
+            }
+            return await badgeAD.RegistrarBadge(badge); // Sin await si no es asíncrono
+        }
+
+
+
+
+
+
+
+    }
+
+    
+
+
+
+}
