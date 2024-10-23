7f3075e1a8fcc3589fc7789329aee1104cd08d9e
EdwinLF <117051836+EdwinLF@users.noreply.github.com>
2024-10-23 11:11:08 -0600

    feature
    Listo los cambios en el ingresar un rango horario

--- GestionAsistentes.BlazorUI/GestionAsistentes.BlazorUI/Controlador/HorarioControlador.cs
+++ GestionAsistentes.BlazorUI/GestionAsistentes.BlazorUI/Controlador/HorarioControlador.cs
@@ -3,6 +3,7 @@ using GestionAsistentes.ReglasNegocio;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace GestionAsistentes.BlazorUI.Controlador
 {
     public class HorarioControlador
@@ -17,10 +18,10 @@ namespace GestionAsistentes.BlazorUI.Controlador
         {
             return await horarioRN.RegistrarHorario(horario);
         }
-        public async Task<List<Horario>> ListarHorariosPorEstacionTrabajo(int estacionTrabajoID)
+        public async Task<List<Horario>> ListarHorariosPorEstacionTrabajo(int estacionTrabajoID, string dia)
         {
             this.horarios = await horarioRN.ListarHorariosPorEstacionTrabajo(estacionTrabajoID);
-            editarHorasDisponibles(estacionTrabajoID);
+            editarHorasDisponibles(dia);
             return await horarioRN.ListarHorariosPorEstacionTrabajo(estacionTrabajoID);
         }
         // Lista de días de la semana
@@ -32,21 +33,21 @@ namespace GestionAsistentes.BlazorUI.Controlador
         // Lista de horas (de 7 a 20)
         public List<string> horasDelDiaInicio = new();
         public List<string> horasDelDiaFinal = new();
-        public async Task<bool> editarHorasDisponibles(int estacionTrabajoID)
+        public async Task editarHorasDisponibles(string dia)
         {
             var todasLasHoras = new List<string>
-    {
-        "07:00", "08:00", "09:00", "10:00", "11:00", "12:00",
-        "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"
-    };
+            {
+                "07:00", "08:00", "09:00", "10:00", "11:00", "12:00",
+                "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"
+            };
 
             // Inicializa las listas de horas disponibles
             this.horasDelDiaInicio = new List<string>(todasLasHoras);
             this.horasDelDiaFinal = new List<string>(todasLasHoras);
-
+            Console.WriteLine(dia);
             // Obtiene las horas ocupadas
             var horasOcupadas = horarios
-                .Where(h => h.EstacionTrabajoID == estacionTrabajoID)
+                .Where(h => h.Dia == dia)
                 .Select(h => new
                 {
                     Inicio = h.HoraInicio.TimeOfDay,
@@ -63,19 +64,25 @@ namespace GestionAsistentes.BlazorUI.Controlador
                 foreach (var hora in todasLasHoras)
                 {
                     var horaActual = TimeSpan.Parse(hora);
-                    if (horaActual >= ocupado.Inicio && horaActual < ocupado.Fin)
+                    if (horaActual >= ocupado.Inicio && horaActual <= ocupado.Fin)
                     {
                         horasNoDisponibles.Add(hora);
                     }
                 }
             }
-
             // Filtra las horas del día de inicio y final
             horasDelDiaInicio = todasLasHoras.Except(horasNoDisponibles).ToList();
             horasDelDiaFinal = todasLasHoras.Except(horasNoDisponibles).ToList();
+        }
+        public async Task verificarHoraSeleccionar(string horaInicio) {
+            horasDelDiaFinal = horasDelDiaInicio;
+            // Convierte la hora de inicio a un formato `TimeSpan` para comparación
+            TimeSpan horaInicioTimeSpan = TimeSpan.Parse(horaInicio);
 
-            return true;
+            // Filtra las horas del día final para eliminar todas las horas menores o iguales a la hora de inicio
+            horasDelDiaFinal = horasDelDiaFinal
+                .Where(h => TimeSpan.Parse(h) > horaInicioTimeSpan)
+                .ToList();
         }
     }
-
 }
