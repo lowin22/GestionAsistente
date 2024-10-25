2328604bf4ecc990fdc24ea45a90240557b2f2e4
waltaa00 <walemisanbre@gmail.com>
2024-10-24 23:30:17 -0600

    Listar Reportes

--- GestionAsistentes.BlazorUI/GestionAsistentes.AccesoDatos/EntidadesAD/ReporteBadgeAD.cs
+++ GestionAsistentes.BlazorUI/GestionAsistentes.AccesoDatos/EntidadesAD/ReporteBadgeAD.cs
@@ -1,6 +1,7 @@
 ﻿using GestionAsistentes.AccesoDatos.Contexto;
 using GestionAsistentes.AccesoDatos.Modelos;
 using GestionAsistentes.Entidades;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace GestionAsistentes.AccesoDatos.EntidadesAD
         {
             this._contexto = new GestionAsistenteContexto();
         }
-        public bool RegistrarReporteBadge(ReporteBadge reporteBadge)
+        public async Task<bool> RegistrarReporteBadge(ReporteBadge reporteBadge)
         {
 
             ReporteBadgeEF reporteBadgeEF = new ReporteBadgeEF
@@ -29,5 +30,29 @@ namespace GestionAsistentes.AccesoDatos.EntidadesAD
             this._contexto.ReporteBadgeEFs.Add(reporteBadgeEF);
             return this._contexto.SaveChanges() > 0;
         }
+
+        public async Task<List<ReporteBadge>> listarReporte()
+        {
+            List<ReporteBadgeEF> reporteEFs = _contexto.ReporteBadgeEFs.ToList();
+            List<ReporteBadge> reportes = new List<ReporteBadge>();
+
+            foreach (ReporteBadgeEF reporteEF in reporteEFs) // Cambiado a UnidadEF
+            {
+                reportes.Add(new ReporteBadge
+                {
+                    NombreUsuario = "Usuario",
+                    NumeroBadge = reporteEF.NumeroBadge,
+                    NombreAsistente = reporteEF.NombreAsistente, 
+                    Fecha = DateTime.Now
+                });
+            }
+
+            return reportes;
+        }
+
+
+
+
+
     }
 }
